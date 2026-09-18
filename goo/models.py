1472f6591534360447bba1d7f6af09bfb708c603
Azizbek <azizbekmutallibov2@gmail.com>
2026-09-18 15:02:53 +0500

    model va view qo'shildi

--- goo/models.py
+++ goo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title[0:100]  # Return the first 100 characters of the title
+
+    
+
+
+
+
+
--- goo/views.py
+++ goo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Post
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+class PostListView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'post_list.html', {'posts': posts})
+    
+
